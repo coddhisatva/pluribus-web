8adf0863af54dfd5d3e9bbc312b124a8534b4208
Luke Sampson <me@lukesampson.com>
2022-04-15 18:06:16 +1000

    Inline form editing proof of concept for creator profile.

---
+++ public/js/up.js
@@ -0,0 +1,270 @@
+/*
+© Pluribus 2022
+
+Used to wire up multi-section forms with inline editing
+on each section and partial updates via server API calls.
+
+*/
+
+const up = {
+	wire: function(container, model, saveUrl) {
+		var views = container.querySelectorAll('[view]');
+
+		views.forEach(function(view) {
+			var edit = view.nextElementSibling;
+			if(edit.getAttribute('edit') == null) {
+				edit = null; // No edit view
+			}
+
+			if(edit) {
+				// Hook up editing
+				var editBtn = view.querySelector('[edit-btn]');
+				if(editBtn) {
+					editBtn.addEventListener('click', function(e) {
+						e.preventDefault();
+						edit.style.display = 'block';
+						view.style.display = 'none';
+						up.setFormValues(edit, model);
+					});
+				};
+
+				function showView() {
+					edit.style.display = 'none';
+					view.style.display = 'block';
+				}
+
+				var saveBtn = edit.querySelector('[save-btn]');
+				if(saveBtn) {
+					saveBtn.addEventListener('click', function(e) {
+						e.preventDefault();
+						up.saveFormValues(edit, model, saveUrl,
+							function() {
+								// Start saving
+								// TODO: UI to show saving.
+								saveBtn.dataset.text = saveBtn.innerText;
+								saveBtn.innerText = 'Saving...';
+								saveBtn.disabled = true;
+								cancelBtn.style.display = 'none';
+							},
+							function(data, err) {
+								// Finish saving
+								saveBtn.innerText = saveBtn.dataset.text;
+								saveBtn.disabled = false;
+								delete cancelBtn.style.display;
+								if(err) {
+									alert('Error saving form!' + err);
+								} else {
+									for(var prop in data) {
+										model[prop] = data[prop];
+									}
+									view.update();
+								}
+								showView();
+							});
+					});
+				}
+
+				var cancelBtn = edit.querySelector('[cancel-btn]');
+				if(cancelBtn) {
+					cancelBtn.addEventListener('click', function(e) {
+						e.preventDefault();
+						showView();
+					})
+				}
+			}
+
+			// Model bindings
+			var boundElements = view.querySelectorAll('[bind]');
+			view.bind = function() {
+				boundElements.forEach(function(boundElement) {
+					var bind = boundElement.getAttribute('bind');
+					var result;
+					try {
+						result = up.eval(model, bind);
+					} catch(e) {
+						throw 'Error evaluating bind="' + bind + '": ' + e;
+					}
+					
+					boundElement.innerHTML = result;
+				});
+			};
+
+			// Conditionals
+			const conditionTypes = ['if','else-if','else'];
+			var conditionalSelector = '[' + conditionTypes.join('],[') + ']';
+			var conditionalElements = view.querySelectorAll(conditionalSelector);
+			view.doConditionals = function() {
+				conditionalElements.forEach(function(conditionalElement) {
+					var conditionType = null;
+					var condition = null;
+					for(var i in conditionTypes) {
+						condition = conditionalElement.getAttribute(conditionTypes[i]); // will be empty string for else
+						if(condition != null) {
+							conditionType = conditionTypes[i];
+							break;
+						}
+					}
+					
+					var visible;
+					if(conditionType == 'if') {
+						visible = up.evalCondition(model, condition, conditionType);
+					} else if(conditionType == 'else-if') {
+						var prevElement = conditionalElement.previousElementSibling;
+						if(!prevElement.hasAttribute('if')) {
+							throw 'Conditional else-if element without an if';
+						}
+						visible = prevElement.style.display == 'none' && up.evalCondition(model, condition, conditionType);
+					} else if(conditionType == 'else') {
+						var prevElement = conditionalElement.previousElementSibling;
+						if(!prevElement.hasAttribute('if') && !prevElement.hasAttribute('else-if')) {
+							throw 'Conditional else element without an if or else-if';
+						}
+						visible = prevElement.style.display == 'none';
+					}
+
+					conditionalElement.style.display = visible ? '' : 'none';
+				});
+			};
+
+			view.update = function() {
+				view.bind();
+				view.doConditionals();
+			};
+		});
+
+		container.update = function() {
+			views.forEach(function(view) {
+				view.update();
+			});
+		}
+
+		container.update();
+		
+	},
+
+	/** Basic lexer function that differentiates between strings and code. */
+	lex: function(code) {
+		var blockStart = 0;
+		var blockType = 'code';
+
+		var blocks = []
+
+		const stringTypes = {
+			'"': 'dquote-string',
+			"'": 'squote-string',
+			'`': 'backtick-string'
+		}
+
+		for(var i  = 0; i < code.length; i++) {
+			var c = code[i];
+
+			for(var stringStartChar in stringTypes) {
+				if(c == stringStartChar) {
+					var stringType = stringTypes[stringStartChar];
+					if(blockType == stringType && i > 0 && code[i-1] != '\\') {
+						// end block
+						blocks.push([blockType, code.substring(blockStart, i+1)]);
+						blockType = 'code';
+						blockStart = i+1;
+					} else {
+						// start block
+						blocks.push([blockType, code.substring(blockStart, i)]);
+						blockType = stringType;
+						blockStart = i;
+					}
+				}
+			}
+		}
+
+		// remaining code block
+		if(blockStart < code.length - 1) {
+			if(blockType != 'code') {
+				throw 'Unterminated string starting at position ' + blockStart + ': ' + code;
+			}
+	
+			blocks.push([blockType, code.substring(blockStart, code.length)]);
+		}
+		
+
+		return blocks;
+	},
+
+	eval: function(model, code) {
+		// Convert all identifiers to model.identifier for security
+		const allowedKeywords = [ 'if', 'else', 'var', 'let', 'const' ];
+		const reIdentifier = /(?<=^|\s)[\w$_][^\s\.\[\()]*/g;
+
+		var blocks = up.lex(code);
+		var safeCode = '';
+		blocks.forEach(function(block) {
+			var type = block[0];
+			var text = block[1];
+
+			if(type == 'code') {
+				safeCode += text.replace(reIdentifier, function(identifier) {
+					if(allowedKeywords.indexOf(identifier) != -1) { return identifier }
+					return 'model.' + identifier;
+				});
+			} else {
+				safeCode += text;
+			}
+		})
+		
+		return Function('model', 'return ' + safeCode)(model);
+	},
+
+	// Helper functions
+	evalCondition: function(model, condition, conditionType) {
+		try {
+			return up.eval(model, condition);
+		} catch(e) {
+			throw 'Error evaluating ' + conditionType + '="' + condition + '": ' + e;
+		}
+	},
+
+	setFormValues: function(edit, model) {
+		var inputs = edit.querySelectorAll('input,textarea');
+		inputs.forEach(function(input) {
+			if(input.hasAttribute('name')) {
+				var name = input.getAttribute('name');
+				if(model.hasOwnProperty(name)) {
+					input.value = model[name];
+				} else {
+					input.value = '';
+				}
+			}
+		});
+	},
+
+	saveFormValues: function(edit, model, saveUrl, savingCb, savedCb) {
+		var data = new FormData();
+		var updated = { };
+		var inputs = edit.querySelectorAll('input,textarea');
+		inputs.forEach(function(input) {
+			if(input.hasAttribute('name')) {
+				var name = input.getAttribute('name');
+				data.append(name, input.value);
+				updated[name] = input.value; // Save for updating the model if request succeeds
+			}
+		});
+
+		savingCb();
+		fetch(saveUrl, { method: 'post', body: data })
+			.then(response => {
+				for(var prop in updated) {
+					model[prop] = updated[prop];
+				}
+				savedCb();
+			});
+	},
+}
+
+
+// Polyfills (don't know if this is needed)
+if(!Array.prototype.forEach) {
+	Array.prototype.forEach = function(callback) {
+		for(var i = 0; i < this.length; i++) {
+			callback(this[i], i, this);
+		}
+	}
+}
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const { body, validationResult } = require('express-validator');
 const auth = require('../utils/auth');
 const { User, Creator, Follow } = require('../models');
+const multer = require('multer');
+const upload = multer();
 
 router.get('/', auth.authorize, async function(req, res, next) {
 	var user = await User.findByPk(req.authUser.id);
@@ -10,6 +12,34 @@ router.get('/', auth.authorize, async function(req, res, next) {
 	console.log(following);
 
 	res.render('dashboard/index', { user, following });
+});
+
+router.get('/profile', auth.authorize, async function(req, res, next) {
+	var user = await User.findByPk(req.authUser.id);
+	var creator = await Creator.findOne({ where: { userId: user.id }});
+	if(!creator) {
+		res.status(404);
+		return;
+	}
+
+	res.render('dashboard/profile', { user, creator });
+});
+
+router.post('/profile', auth.authorize, upload.none(), async function(req, res, next) {
+	var user = await User.findByPk(req.authUser.id);
+	var creator = await Creator.findOne({ where: { userId: user.id }});
+
+	var update = { };
+	['name', 'about'].forEach(prop => {
+		var value = req.body[prop];
+		if(value) {
+			update[prop] = value;
+		}
+	});
+	
+	await Creator.update(update, { where: { id: creator.id }});
+
+	res.send('OK');
 });
 
 module.exports = router;
