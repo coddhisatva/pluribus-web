0e536f6c1d1670892d9164951dd1d3b3c4a5b33d
Luke Sampson <me@lukesampson.com>
2022-06-05 05:38:35 +1000

    Add 'About Us' page content.

--- routes/index.js
+++ routes/index.js
@@ -25,4 +25,8 @@ router.get('/how-it-works', function(req, res) {
 	res.render('how-it-works');
 });
 
+router.get('/about', function(req, res) {
+	res.render('about');
+})
+
 module.exports = router;
