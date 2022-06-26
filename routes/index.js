37da0b12f05deed362d16f9e3ddf6b746d919b10
Luke Sampson <me@lukesampson.com>
2022-06-26 19:57:10 +1000

    Implement newsletter signup.

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 const { redirect } = require('express/lib/response');
 const { body, validationResult } = require('express-validator');
 var router = express.Router();
-var { Creator } = require('../models');
+var { Creator, PrelaunchEmail } = require('../models');
 
 /**
  * GET /
@@ -17,9 +17,39 @@ router.get('/', async function(req, res, next) {
 	}
 
 	// Home page
-	creatorCount = await Creator.count();
-	res.render('index', { creatorCount });
+	//creatorCount = await Creator.count();
+	res.render('index');
 });
+
+/** POST /newsletter(email: string) */
+router.post('/newsletter',
+	body('email').trim().isLength({min:1}).withMessage('Please enter your email address').bail()
+		.isEmail().withMessage('Please enter a valid email address'),
+	async function(req, res, next) {
+		const errors = validationResult(req);
+		if(!errors.isEmpty()) {
+			req.flash.alert = "Please enter a valid email address!";
+			res.render('index');
+			return;
+		}
+
+		var email = req.body.email;
+		var existing = await PrelaunchEmail.findOne({ where: { email: email }});
+		if(existing) {
+			req.flash.notice = "You've already subscribed to the newsletter.";
+			res.render('index');
+			return;
+		}
+
+		await PrelaunchEmail.create({ email });
+
+		res.redirect('newsletter-signup')
+	}
+);
+
+router.get('/newsletter-signup', (req, res) => {
+	res.render('newsletter-signup');
+})
 
 router.get('/how-it-works', function(req, res) {
 	res.render('how-it-works');
