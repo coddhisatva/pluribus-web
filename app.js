3b152e51c899e770ab55bd79344fdc2023785fff
Luke Sampson <me@lukesampson.com>
2023-07-12 15:09:06 +1000

    Show dedicated 404 page instead of the server error handler.

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ app.use('/stripe', stripeRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-	next(createError(404));
+	res.render('notfound');
 });
 
 // error handler
