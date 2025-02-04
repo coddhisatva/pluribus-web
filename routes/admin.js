391d12ee244916fb6d9a8d72fbc63a9ed026961c
Conor Egan <conoregan151@gmail.com>
2025-02-03 16:21:50 -0800

    maybe should be able to login as admin now (temporary session-based user)

--- routes/admin.js
+++ routes/admin.js
@@ -2,42 +2,12 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../utils/auth');
 const csrf = require('../utils/csrf');
-const credentials = require('../config/credentials');
 const email = require('../utils/email');
 require('../utils/handleAsyncErrors').fixRouter(router);
 const { Creator, User, sequelize, PolicyExecution } = require('../models');
 
-router.all('*', (req, res, next) => {
-	if(!/log(?:in|out)?/.test(req.path)) {
-		auth.authorizeRole('admin', '/admin/login')(req, res, next);
-		return;
-	}
-	next();
-});
-
-router.get('/login', function(req, res, next) {
-	res.render('admin/login');
-});
-
-router.post('/login', csrf.validateToken, function(req, res, next) {
-	var errors = [];
-
-	if(req.body.email != credentials.admin.username) {
-		errors.push({ msg: 'Unknown user', param: 'email' });
-	}
-
-	if(!auth.verifyPassword(req.body.password, credentials.admin.password)) {
-		errors.push({ msg: 'Invalid password', param: 'password'});
-	}
-
-	if(errors.length > 0) {
-		res.render('admin/login', { errors });
-		return;
-	}
-
-	req.session.authUser = { email: req.body.email, roles: [ 'admin' ] };
-	res.redirect('/admin');
-});
+// Simplify to just role check
+router.all('*', auth.authorizeRole('admin'));
 
 router.get('/', async function(req, res, next) {
 	res.render('admin/index');
